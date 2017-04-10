c0e068030b49f174f966f66de28ca683d43cb3e9
Ben Fairbanks <bdfairbanks3@gmail.com>
2017-04-10 16:53:15 -0500

    js functions created

--- category-public-work.php
+++ category-public-work.php
@@ -1,16 +1,19 @@
-      <?php $art = array(); ?>
+ <head>
+  <script src="<?php bloginfo('template_directory'); ?>/slideshow.js"></script>
+ </head>
+      <?php $counter = 0; ?>
 <?php if ( have_posts() ) : ?>
-<ul>
-    <?php wp_list_categories( array(
-    ) ); ?> 
-</ul>
+
 <?php
   // Start the loop.
   while ( have_posts() ) : the_post();
   ?>
 
-  <?php  array_push($art, get_field( 'the_art' )["url"]); ?>
+  <?php $art = get_field( 'the_art' )["url"]; ?>
 
+  <div style="width: 75%">
+  <img src=<?php echo $art?> class="slideshow" style="height: 400px; display: none;">
+  
   <?php
   // End the loop.
   endwhile;
@@ -18,10 +21,14 @@
 endif;
 ?>
 
-<script hidden class="echo_num"> <?php echo count($art) ?> </script> 
+<button class="prev"> Prev </button>
+<button class="next"> Next </button> 
+</div>
+<!-- 
+ <p hidden class="echo_num"> <?php echo htmlspecialchars( count($art) ); ?> </p>
 
  <div style="width: 75%">
     <img src=<?php echo $art[0] ?> class="slideshow" style="height: 400px">
       <button class="prev"> Prev </button>
       <button class="next"> Next </button>   
-  </div>
+  </div> -->
--- slideshow.js
+++ slideshow.js
@@ -1,40 +1,92 @@
 window.onload = function(){
-  
-  var slideIndex = getElementsByClassName("echo_num").innerHTML.to_i; 
-  debugger
-  showSlides(slideIndex);
+ready_next_button;
+ready_prev_button;
+display_on_load;
 
 
-  prime();
-
-  function prime(){
-  document.getElementsByClassName("prev")[0].addEventListener("click", minusSlides);
-   document.getElementsByClassName("next")[0].addEventListener("click", plusSlides);
-  }
-
-  function minusSlides(){
-    n = slideIndex -1
-    showSlides(n);
-  }
-
-  function plusSlides() {
-    n = slideIndex + 1
-    showSlides(n);
-  }
-
-  function currentSlide(n) {
-    showSlides(slideIndex = n);
-  }
-
-  function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("mySlide");
-    debugger
-    if (n > slides.length) {slideIndex = 1} 
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none"; 
-    }
-    slides[slideIndex-1].style.display = "block"; 
-  }
 }
+
+function painting_tracker_baseline(){
+ return document.getElementsByClassName("slideshow").length
+}
+
+function display_on_load(){
+document.getElementsByClassName("slideshow")[0].style.display = "block";   
+}
+
+function ready_next_button(){
+ document.getElementsByClassName("next")[0].addEventListener("click", plusSlides);  
+}
+
+function ready_prev_button(){
+ document.getElementsByClassName("prev")[0].addEventListener("click", minusSlides);
+}
+
+function plusSlides(numberTracker){
+document.getElementsByClassName(slideshow)[numberTracker].class.display = "none";
+document.getElementsByClassName(slideshow)[numberTracker +=1].class.display= "block";
+add_to_tracker
+}
+
+function minusSlides(numberTracker){
+document.getElementsByClassName(slideshow)[numberTracker].class.display = "none";
+document.getElementsByClassName(slideshow)[numberTracker +=-1].class.display = "block";
+subtract_from_tracker
+}
+
+function add_to_tracker(numberTracker){
+  numberTracker +=1;
+}
+
+function subtract_from_tracker(numberTracker){
+  numberTracker += -1;
+}
+
+//   var slideIndex = document.getElementsByClassName("echo_num")[0].innerHTML; 
+//   debugger
+//   showSlides(slideIndex);
+
+
+//   prime();
+
+//   function prime(){
+//   document.getElementsByClassName("prev")[0].addEventListener("click", minusSlides);
+//    document.getElementsByClassName("next")[0].addEventListener("click", plusSlides);
+//   }
+
+  
+
+
+
+
+
+
+
+
+
+//   function minusSlides(){
+//     n = slideIndex -1
+//     showSlides(n);
+//   }
+
+//   function plusSlides() {
+//     n = slideIndex + 1
+//     showSlides(n);
+//   }
+
+//   function currentSlide(n) {
+//     showSlides(slideIndex = n);
+//   }
+
+//   function showSlides(n) {
+//     var i;
+//     var slides = document.getElementsByClassName("mySlide");
+//     debugger
+//     if (n > slides.length) {slideIndex = 1} 
+//     if (n < 1) {slideIndex = slides.length}
+//     for (i = 0; i < slides.length; i++) {
+//         slides[i].style.display = "none"; 
+//     }
+//     slides[slideIndex-1].style.display = "block"; 
+//   }
+// }
