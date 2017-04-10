e57b6fa848a402633a623cf15ea3d212804f20d4
Aaron Steward <aa.daly.stew@gmail.com>
2017-04-10 11:39:53 -0500

    initial commit

---
+++ header.php
@@ -0,0 +1,12 @@
+<?php?>
+
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+    <head>
+        <meta charset="<?php bloginfo( 'utf-8' ); ?>" />
+        <title><?php wp_title(); ?></title> 
+        <link href="<?php bloginfo('template_directory'); ?>/style.css" rel="stylesheet">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <?php wp_head(); ?>
+    </head>
---
+++ home.php
@@ -0,0 +1,9 @@
+<?php get_header(); ?>
+
+<div class="landing-image">
+	<div id="button-wrapper">
+		<a href="/index" class="btn landing-button">Watie White</a>
+	</div>
+</div>
+
+<?php get_footer(); ?>
