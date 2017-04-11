9725192ed20c0963652002071bf6540b2eed18d2
Aaron Steward <aa.daly.stew@gmail.com>
2017-04-11 10:33:15 -0500

    beginning sidebar

---
+++ sidebar.php
@@ -0,0 +1,20 @@
+<div class="sidebar-wrapper">
+
+	<div class="logo">
+		<img src="#" href="/" alt="Watie White">
+	</div>
+	<div class="sidebar-top">
+		<ul class="galleries">
+			<li><a href="/">Home</a></li>
+			<li><a href="/category/studio-work">Studio Work</a></li>
+			<li><a href="/category/public-work">Public Art</a></li>
+		</ul>
+	</div>
+	<div class="sidebar-bottom">
+		<ul class="publicity">
+			<li><a href="/cv">CV</a></li>
+			<li><a href="/contact">Contact</a></li>
+			<li><a href="/press">Press</a></li>
+		</ul>
+	</div>
+</div>
