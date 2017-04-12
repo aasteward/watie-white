e6957154898740b6c5e179ce9edaa64607ebbf02
Ben Fairbanks <bdfairbanks3@gmail.com>
2017-04-12 13:46:57 -0500

    spacing fixed?

--- category-public-work.php
+++ category-public-work.php
@@ -1,36 +1,45 @@
 <?php get_header(); ?>
-    <?php if ( have_posts() ) : ?>
+    <div class="page-wrapper">
+      <div class="side-content">
+        <?php get_sidebar(); ?>
+      </div>
+      <div class="main-content">
+        <?php if ( have_posts() ) : ?>
+        <?php
+        // Start the loop.
+        while ( have_posts() ) : the_post();
+        ?>
 
-    <?php
-      // Start the loop.
-      while ( have_posts() ) : the_post();
-      ?>
+        <?php $art = get_field( 'the_art' )["url"]; ?>
+        <?php $title = get_field( 'title' ) ?>
+        <?php $series = get_field( 'series' ) ?>
+        <?php $media = get_field( 'media' ) ?>
+        <?php $size = get_field( 'size' ) ?>
+        <?php $year = get_field( 'year' ) ?>
 
-      <?php $art = get_field( 'the_art' )["url"]; ?>
-      <?php $title = get_field( 'title' ) ?>
-      <?php $series = get_field( 'series' ) ?>
-      <?php $media = get_field( 'media' ) ?>
-      <?php $size = get_field( 'size' ) ?>
-      <?php $year = get_field( 'year' ) ?>
+        <div style="width: 75%">
+          <img src=<?php echo $art ?> class="slideshow" style="display: none; height: 400px">
 
-      <div style="width: 75%">
-        <img src=<?php echo $art ?> class="slideshow" style="display: none; height: 400px">
-      
-        <div class="slideshow_info_display" style="display: none;">
+          <div class="slideshow_info_display" style="display: none;">
           <?php echo $title ?>
           <?php echo $series ?>
           <?php echo $media ?>
           <?php echo $size ?>
           <?php echo $year ?>
-        </div>
-      <?php
-      endwhile;
-      endif;
-      ?>
+          </div>
 
-      <button class="prev" data-direction="prev"> Prev </button>
-      <button class="next" data-direction="next"> Next </button> 
+          <?php
+          // End the loop.
+          endwhile;
+
+          endif;
+          ?>
+
+          <button class="prev" data-direction="prev"> Prev </button>
+          <button class="next" data-direction="next"> Next </button> 
+        </div>
+      </div>
     </div>
   </div>
 </body>
-<?php get_footer(); ?> 
+<?php get_footer(); ?>
